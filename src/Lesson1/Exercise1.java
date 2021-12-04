3dcc5a41fdede47d412b66758e4183d4f6c1643f
@poluhin <poluhin@i-teco.ru>
2021-12-04 15:39:48 +0300

    task 1

---
+++ src/Lesson1/Exercise1.java
@@ -0,0 +1,24 @@
+package Lesson1;
+
+public class Exercise1 {
+public static void main (String [] args)
+{
+    String a = "___________________\'\"___";
+    String b = "__________________---___";
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println("_____________/\\__|__|___");
+    System.out.println("____________/__\\_|__|___");
+    System.out.println("___________/____\\|__|___");
+    System.out.println("__________/______\\__|___");
+    System.out.println("_________/________\\_|___");
+    System.out.println("________/__________\\|___");
+    System.out.println("_______/____[__]____\\___");
+    System.out.println("______/______________\\__");
+    System.out.println("_____/|______________|\\_");
+    System.out.println("_______|_____---____|___");
+    System.out.println("_______|_____| |____|___");
+
+}
+
+}
